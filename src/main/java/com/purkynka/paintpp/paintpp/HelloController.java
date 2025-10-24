9a44db7bc147c1c90f68056e7143c82ab1975145
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-10-24 20:02:32 +0200

    Initial commit

---
+++ src/main/java/com/purkynka/paintpp/paintpp/HelloController.java
@@ -0,0 +1,14 @@
+package com.purkynka.paintpp.paintpp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
