76e605a8c9117cd56b3f2b7fb708ba5ce4d8fe19
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-10-24 20:03:59 +0200

    Remove JavaFX FXML dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,5 @@
 module com.purkynka.paintpp.paintpp {
     requires javafx.controls;
-    requires javafx.fxml;
 
-
-    opens com.purkynka.paintpp.paintpp to javafx.fxml;
     exports com.purkynka.paintpp.paintpp;
 }
