d59a742d2c87b2ef14c849088ae02f12290224e9
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-10-24 21:33:22 +0200

    Add Ikonli and Ikonli Material Design 2 dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.purkynka.paintpp.paintpp {
     requires javafx.controls;
     requires atlantafx.base;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.materialdesign2;
 
     exports com.purkynka.paintpp;
 }
