b6a39265517e13a92b619fe406f3667a9fdc8e28
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-10-25 21:08:40 +0200

    Add crude ImageViewer implementation

--- src/main/java/com/purkynka/paintpp/ui/primarystage/mainview/imageviewer/ImageViewer.java
+++ src/main/java/com/purkynka/paintpp/ui/primarystage/mainview/imageviewer/ImageViewer.java
@@ -2,15 +2,24 @@ package com.purkynka.paintpp.ui.primarystage.mainview.imageviewer;
 
 import com.purkynka.paintpp.logic.event.ConsumerEventHandler;
 import com.purkynka.paintpp.logic.image.imageprovider.IImageProvider;
-import com.purkynka.paintpp.ui.shared.TemporaryPane;
+import javafx.geometry.Pos;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
-public class ImageViewer extends TemporaryPane {
+public class ImageViewer extends StackPane {
     private IImageProvider imageProvider;
+
+    private ImageView imageView;
 
     public static ConsumerEventHandler<IImageProvider> IMAGE_PROVIDER_CHANGED = new ConsumerEventHandler<>();
 
     public ImageViewer() {
-        super("Image Viewer");
+        super();
+
+        imageView = new ImageView();
+        StackPane.setAlignment(imageView, Pos.CENTER);
+
+        getChildren().addAll(imageView);
 
         IMAGE_PROVIDER_CHANGED.addEventListener(this::onImageProviderChanged);
     }
@@ -20,5 +29,7 @@ public class ImageViewer extends TemporaryPane {
         refreshImageView();
     }
 
-    private void refreshImageView() {}
+    private void refreshImageView() {
+        imageView.setImage(imageProvider.getImage());
+    }
 }
