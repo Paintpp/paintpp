272bafff7f1c5e3462d2c15c04307fb90827e265
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-10-28 00:11:26 +0100

    Code cleanup after merge

--- src/main/java/com/purkynka/paintpp/mainview/panes/ColorPickerPane.java
+++
@@ -1,7 +0,0 @@
-package com.purkynka.paintpp.mainview.panes;
-
-public class ColorPickerPane extends PlaceholderPane {
-    public ColorPickerPane() {
-        super("Color Picker");
-    }
-}
--- src/main/java/com/purkynka/paintpp/mainview/panes/LayersAndFiltersPane.java
+++
@@ -1,7 +0,0 @@
-package com.purkynka.paintpp.mainview.panes;
-
-public class LayersAndFiltersPane extends PlaceholderPane {
-    public LayersAndFiltersPane() {
-        super("Layers and Filters");
-    }
-}
