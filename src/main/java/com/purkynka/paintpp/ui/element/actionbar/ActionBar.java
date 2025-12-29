fae9535c2fa92b54eaeb7c9e1e87a71833706e72
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-12-29 20:06:20 +0100

    Add action bar

--- src/main/java/com/purkynka/paintpp/ui/element/actionbar/ActionBar.java
+++ src/main/java/com/purkynka/paintpp/ui/element/actionbar/ActionBar.java
@@ -1,9 +1,12 @@
 package com.purkynka.paintpp.ui.element.actionbar;
 
 import com.purkynka.paintpp.ui.CommonCSS;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import org.kordamp.ikonli.materialdesign2.MaterialDesignC;
+import org.kordamp.ikonli.materialdesign2.MaterialDesignF;
+import org.kordamp.ikonli.materialdesign2.MaterialDesignH;
 
 public class ActionBar extends VBox {
     public static final double ACTION_BAR_WIDTH = 96d;
@@ -14,8 +17,17 @@ public class ActionBar extends VBox {
 
         this.getStyleClass().add("action-bar");
 
-        HBox.setHgrow(this, Priority.NEVER);
         this.setMinWidth(ACTUAL_ACTION_BAR_WIDTH);
         this.setMaxWidth(ACTUAL_ACTION_BAR_WIDTH);
+
+        var filler = new Pane();
+        VBox.setVgrow(filler, Priority.ALWAYS);
+
+        this.getChildren().addAll(
+                new ActionBarButton(MaterialDesignF.FUNCTION_VARIANT, "Add Filter"),
+                filler,
+                new ActionBarButton(MaterialDesignC.COG, "Settings"),
+                new ActionBarButton(MaterialDesignH.HELP, "Help")
+        );
     }
 }
