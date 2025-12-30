506864023f0a181a17e1b1559176005b71ec1a2e
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-12-30 03:04:37 +0100

    Add popup stages, move stylesheet adding to common method

--- src/main/java/com/purkynka/paintpp/ui/CommonCSS.java
+++ src/main/java/com/purkynka/paintpp/ui/CommonCSS.java
@@ -1,5 +1,25 @@
 package com.purkynka.paintpp.ui;
 
+import com.purkynka.paintpp.ui.stage.main.MainStage;
+import javafx.scene.Scene;
+
+import java.util.Objects;
+
 public class CommonCSS {
     public static final double BORDER_WIDTH = 2;
+
+    public static void addStylesheetsToScene(Scene scene) {
+        scene.getStylesheets().addAll(
+                CommonCSS.getStyleSheet("/colors.css"),
+                CommonCSS.getStyleSheet("/menu_bar.css"),
+                CommonCSS.getStyleSheet("/main_root.css"),
+                CommonCSS.getStyleSheet("/action_bar.css"),
+                CommonCSS.getStyleSheet("/status_bar.css"),
+                CommonCSS.getStyleSheet("/popup.css")
+        );
+    }
+
+    private static String getStyleSheet(String relativePath) {
+        return Objects.requireNonNull(MainStage.class.getResource("/com/purkynka/paintpp/css" + relativePath)).toExternalForm();
+    }
 }
--- src/main/java/com/purkynka/paintpp/ui/stage/main/MainStage.java
+++ src/main/java/com/purkynka/paintpp/ui/stage/main/MainStage.java
@@ -1,9 +1,8 @@
 package com.purkynka.paintpp.ui.stage.main;
 
+import com.purkynka.paintpp.ui.CommonCSS;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.util.Objects;
 
 public class MainStage {
     public static Stage MAIN_STAGE;
@@ -16,19 +15,9 @@ public class MainStage {
         MainStage.MAIN_STAGE.setMinHeight(800);
 
         var scene = new Scene(new MainRoot());
-        scene.getStylesheets().addAll(
-                this.getStyleSheet("/colors.css"),
-                this.getStyleSheet("/menu_bar.css"),
-                this.getStyleSheet("/main_root.css"),
-                this.getStyleSheet("/action_bar.css"),
-                this.getStyleSheet("/status_bar.css")
-        );
+        CommonCSS.addStylesheetsToScene(scene);
 
         MainStage.MAIN_STAGE.setScene(scene);
-    }
-
-    private String getStyleSheet(String relativePath) {
-        return Objects.requireNonNull(MainStage.class.getResource("/com/purkynka/paintpp/css" + relativePath)).toExternalForm();
     }
 
     public void show() {
